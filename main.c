256c558832fbd96156cdbb3d83f2fb23295b45a2
Adrian Freeman <156028590+adrianfree01@users.noreply.github.com>
2025-07-16 20:20:12 -0300

    logo + original main.c
    added gummyBear.bmp and my original main.c (oops)

--- main.c
+++ main.c
@@ -1,46 +1,29 @@
-#include <stdio.h>
 #include <SDL3/SDL.h>
+#include <stdio.h>
 
 #define gameboyWidth 160
 #define gameboyHeight 144
 
 #define gummyBearWidth gameboyWidth*4
 #define gummyBearHeight gameboyHeight*4
-
-int main()
-{
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Window* win = SDL_CreateWindow("gummyBear", gummyBearWidth, gummyBearHeight, 0);
-    if (!win)
-    {
-        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
+int main() {
+    
+    SDL_Window* win = SDL_CreateWindow("gummyBear", gummyBearWidth, gummyBearHeight, SDL_WINDOW_MAXIMIZED);
+    SDL_Renderer *renderer = SDL_CreateRenderer(win, NULL);
     SDL_Event event;
     int running = 1;
-
     while(running)
     {
         while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
+        {  
+            switch(event.type) {
                 case SDL_EVENT_QUIT:
+                    SDL_DestroyWindow(win);
+                    SDL_Quit();
                     running = 0;
                     break;
             }
         }
-        SDL_Delay(16);
     }
-
-    SDL_DestroyWindow(win);
-    SDL_Quit;
     return 0;
 }
